2fd527b5d33701ef4c54dbf7c25204100b268c1a
wingjl28 <wingjl28@gmail.com>
2020-05-11 18:39:53 -0500

    Dice class created per TestDice TDD Junit tests.

---
+++ SEIS635FinalProject/src/com/skunk/Dice.java
@@ -0,0 +1,43 @@
+package com.skunk;
+
+public class Dice
+{
+
+	private int lastRoll;
+	private Die die1;
+	private Die die2;
+	
+	Dice() {
+		this.die1 = new Die();
+		this.die2 = new Die();
+		this.roll();
+	}
+
+	Dice(int dieOne, int dieTwo){
+		this.die1 = new Die(dieOne);
+		this.die2 = new Die(dieTwo);
+		this.roll();
+	}
+
+	public int getLastRoll()
+	{
+		return this.lastRoll;
+	}
+
+	public void roll()
+	{
+		die1.roll();
+		die2.roll();
+		this.lastRoll = die1.getLastRoll() + die2.getLastRoll();
+	}	
+
+	public Die getDie1() {
+		return die1;
+	}
+
+	public Die getDie2() {
+		return die2;
+	}
+
+
+}
