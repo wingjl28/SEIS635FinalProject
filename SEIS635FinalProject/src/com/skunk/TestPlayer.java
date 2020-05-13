3bef4b06a152905e2e43e2d933502fc423e33047
wingjl28 <wingjl28@gmail.com>
2020-05-13 07:05:25 -0500

    Update player to remove responsibilities that Turn has taken over.
    Update player test to align with changes to Turn and Player classes.

--- SEIS635FinalProject/src/com/skunk/TestPlayer.java
+++ SEIS635FinalProject/src/com/skunk/TestPlayer.java
@@ -19,16 +19,11 @@ class TestPlayer {
 		assertEquals(playerOne.getGameScore(), 0);
 		
 		//test adding score to player
-		assertEquals(playerOne.getTurnScore(), 0);
-		playerOne.addTurnScore(50);
-		assertEquals(playerOne.getTurnScore(), 50);
-		playerOne.addGameScore();
+		assertEquals(playerOne.getGameScore(), 0);
+		playerOne.addScore(50);
 		assertEquals(playerOne.getGameScore(), 50);
-		
-		playerOne.newTurn();
-		assertEquals(playerOne.getTurnScore(),0);
-		assertEquals(playerOne.getGameScore(), 50);
-			
+
+	
 
 	}
 	
@@ -36,44 +31,42 @@ class TestPlayer {
 	void single_skunk_test() {
 		Player playerOne = new Player("PlayerOne");
 		//add 50 to player score to test single skunk
-		playerOne.addTurnScore(50);
-		playerOne.addGameScore();
+		playerOne.addScore(50);
 		
 		//test single skunk: no change to player game score, 0 turn score, and -1 to chip count.
 		playerOne.singleSkunk();
 		
 		assertEquals(playerOne.getChipCount(), 99);
 		assertEquals(playerOne.getGameScore(), 50);
-		assertEquals(playerOne.getTurnScore(), 0);
+
 	}
 	
 	@Test
 	void double_skunk_test() {
 		Player playerOne = new Player("PlayerOne");
 		//add 50 to player score to test single skunk
-		playerOne.addTurnScore(50);
-		playerOne.addGameScore();
-		
+		playerOne.addScore(50);
+			
 		//test double skunk: 0 game score, 0 turn score, and -4 change to chip count.
 		playerOne.doubleSkunk();
 		
 		assertEquals(playerOne.getChipCount(), 96);
 		assertEquals(playerOne.getGameScore(), 0);
-		assertEquals(playerOne.getTurnScore(), 0);
+		assertEquals(playerOne.getGameScore(), 0);
 	}
 
 	@Test
 	void skunk_deuce_test() {
 		Player skunkDeuce = new Player("PlayerOne");
 		//add 50 to player score to test single skunk
-		skunkDeuce.addTurnScore(50);
-		skunkDeuce.addGameScore();
+		skunkDeuce.addScore(50);
+	
 		
 		//test double skunk: no change to player game score, 0 turn score, and -2 change to chip count.
 		skunkDeuce.skunkDeuce();
 		
 		assertEquals(skunkDeuce.getChipCount(), 98);
 		assertEquals(skunkDeuce.getGameScore(), 50);
-		assertEquals(skunkDeuce.getTurnScore(), 0);
+		
 	}
 }
