4bff45d136474893730bd10b3c8838cf8cdb9545
wingjl28 <wingjl28@gmail.com>
2020-05-13 12:31:58 -0500

    Update servlet controller to allow for game to be played and track scores.

--- SEIS635FinalProject/src/com/skunk/skunkservlet.java
+++ SEIS635FinalProject/src/com/skunk/skunkservlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;  
@@ -38,91 +39,90 @@ public class skunkservlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
 		response.setContentType("text/plain");
 		PrintWriter out = response.getWriter();
-		//HttpSession session = request.getSession();
-		
+		HttpSession session = request.getSession();
+		Game game= (Game) session.getAttribute("game");
+			
 		int action = Integer.parseInt(request.getParameter("button"));
-		
-//		Player playerOneName = new Player(request.getParameter("playerOneName"));
-//		Player playerTwoName = new Player(request.getParameter("playerTwoName"));
-//		Player playerThreeName = new Player(request.getParameter("playerThreeName"));
-//		Player playerFourName = new Player(request.getParameter("playerFourName"));
-//		Player playerFiveName = new Player(request.getParameter("playerFiveName"));
-//		Player playerSixName = new Player(request.getParameter("playerSixName"));
-//		Player playerSevenName = new Player(request.getParameter("playerSevenName"));
-//		Player playerEightName = new Player(request.getParameter("playerEightName"));
-		
-//		Player[] tempPlayers = new Player[8];
-//		tempPlayers[0] = playerOneName;
-//		tempPlayers[1] = playerTwoName;
-//		tempPlayers[2] = playerThreeName;
-//		tempPlayers[3] = playerFourName;
-//		tempPlayers[4] = playerFiveName;
-//		tempPlayers[5] = playerSixName;
-//		tempPlayers[6] = playerSevenName;
-//		tempPlayers[7] = playerEightName;
-//			
-//		ArrayList<Player> players = new ArrayList<Player>();
-//		
-//		for (int i = 0; i < tempPlayers.length; i++) {
-//			if (!tempPlayers[i].getPlayerName().isEmpty()) {
-//				players.add(tempPlayers[i]);
-//			}
-//		}
-		
-		
-		//out.print(playerOneName.getPlayerName());	
 
-		//Game newGame = new Game(players,players.size(), fairDice);
-		
-		//newGame.startGame();
-		
 		if(action == 1) {
 			
+			String playerOneName = request.getParameter("playerOneName");
+			String playerTwoName = request.getParameter("playerTwoName");
+			String playerThreeName = request.getParameter("playerThreeName");
+			String playerFourName = request.getParameter("playerFourName");
+			String playerFiveName = request.getParameter("playerFiveName");
+			String playerSixName = request.getParameter("playerSixName");
+			String playerSevenName = request.getParameter("playerSevenName");
+			String playerEightName = request.getParameter("playerEightName");
 			
-			out.print("<p>Start was pressed </p>");
+			if (game == null) {
+				game = new Game(playerOneName, playerTwoName, playerThreeName, playerFourName, playerFiveName, playerSixName, playerSevenName, playerEightName);
+			    session.setAttribute("game", game);
+			}
 			
-			
-			
-		//out.print("<h3>Current Player:</h3><h3>Roll Result:</h3><h3>Player Scores</h3><p>Player 1:</p>"+ playerOneName.getPlayerName()+ "<p>Player 2:</p>"+playerTwoName.getPlayerName()+"<p>Player 3:</p>"+playerThreeName.getPlayerName()+"<p>Player 4:</p><p>Player 5:</p><p>Player 6:</p><p>Player 7:</p><p>Player 8:</p>");
+			session.setAttribute("game", game);
+			String outputString = "";
+			int playerNum = 0;
+			for (int i = 0; i < game.getPlayers().size(); i++) {
+				playerNum++;
+				outputString = outputString + "<p>Player "+ playerNum +": "+ game.getPlayers().get(i).getPlayerName() + "</p><p>Score: "+game.getPlayers().get(i).getGameScore()+"</p><p>Bank: " +game.getPlayers().get(i).getChipCount()+"</p>" ;
+			}
+			out.print("<p>CURRENT PLAYER: </p><p>" + game.getCurrentPlayerName() + "</p>"); 
+			out.print(outputString + "<p>Number of Players:" + game.getNumberOfPlayers());
 		} 
+		//Roll button pressed.
+		//This should roll for the current player, and perform necessary actions to that player depending on the result of the roll.
 		if(action == 2) {
+			session.setAttribute("game", game);
 			
-			
+			String rollOutput = game.rollTurn();
+			String outputString = "";
+			int playerNum = 0;
+
+			out.print("<p>ROLL RESULT: </p><p>" + rollOutput + "</p>");
+			out.print("<p>TURN SCORE: </p><p>" + game.getTurnScore() + "</p>");
+			if(game.rollAgain()==false) {
+				out.print("<p>NEXT PLAYER: </p><p>" + game.getCurrentPlayerName() + "</p>");
+			}
+			else
+				out.print("<p>CURRENT PLAYER: </p><p>" + game.getCurrentPlayerName() + "</p>");
+			for (int i = 0; i < game.getPlayers().size(); i++) {
+				playerNum++;
+				outputString = outputString + "<p>Player "+ playerNum +": "+ game.getPlayers().get(i).getPlayerName() + "</p><p>Score: "+game.getPlayers().get(i).getGameScore() +"</p><p>Bank: " +game.getPlayers().get(i).getChipCount()+"</p>" ;
+			}
+			out.print(outputString + "<p>Number of Players:" + game.getNumberOfPlayers());
 			out.print("<p>Roll was pressed </p>");
 			
-			
-			
-		//out.print("<h3>Current Player:</h3><h3>Roll Result:</h3><h3>Player Scores</h3><p>Player 1:</p>"+ playerOneName.getPlayerName()+ "<p>Player 2:</p>"+playerTwoName.getPlayerName()+"<p>Player 3:</p>"+playerThreeName.getPlayerName()+"<p>Player 4:</p><p>Player 5:</p><p>Player 6:</p><p>Player 7:</p><p>Player 8:</p>");
 		}
-		
+		//Skip button pressed
+		//This should do no further action to the current player, wrap up their turn, and move to next player.
 		if(action == 3) {
+			session.setAttribute("game", game);
+			game.endTurn();
+			String outputString = "";
+			int playerNum = 0;
+			for (int i = 0; i < game.getPlayers().size(); i++) {
+				playerNum++;
+				outputString = outputString + "<p>Player "+ playerNum +": "+ game.getPlayers().get(i).getPlayerName() + "</p><p>Score: "+game.getPlayers().get(i).getGameScore()+"</p><p>Bank: " +game.getPlayers().get(i).getChipCount()+"</p>" ;
+			}
 			
-			
+			out.print("<p>ROLL RESULT: </p><p>"+game.getPreviousPlayer().getPlayerName()+" has skipped their Roll!</p>");
+			out.print("<p>TURN SCORE: </p><p>" + game.getTurnScore() + "</p>");
+			out.print("<p>NEXT PLAYER: </p><p>" + game.getCurrentPlayerName() + "</p>");
+			out.print(outputString + "<p>Number of Players:" + game.getNumberOfPlayers());
 			out.print("<p>Skip was pressed </p>");
 			
-			
-			
-		//out.print("<h3>Current Player:</h3><h3>Roll Result:</h3><h3>Player Scores</h3><p>Player 1:</p>"+ playerOneName.getPlayerName()+ "<p>Player 2:</p>"+playerTwoName.getPlayerName()+"<p>Player 3:</p>"+playerThreeName.getPlayerName()+"<p>Player 4:</p><p>Player 5:</p><p>Player 6:</p><p>Player 7:</p><p>Player 8:</p>");
 		}
 		
+		//Reset Button pressed.
+		//Clear session and inform user that the game was reset
 		if(action == 4) {
-			
-			out.print("<p>Reset was pressed </p>");
+			session.invalidate();
+			out.print("<p>The game has been reset!</p>");
 		}
 			
-			//tempCounter = (counter) request.getSession().getAttribute("counter");
-			//(counter) session.getAttribute("counter");
-			//counter.removeCount();
-			//out.print("The counter is </p>" + counter.getValue());
-		//out.print("Player 7:</p><p>Player 8:</p>");
-		
-		//out.print("<h5>Monthly Payment:</h5> " + monthlyPayment + "<h5>Total Payment is:</h5> " + totalPayment + "<h5>Total Interest Paid is:</h5> " + interestPayment);
-		
-		
 	
 	}
 
